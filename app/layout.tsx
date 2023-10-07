6b49b78f45925ca1ba0299d13fa1fe36010d30af
daliaabdelwhab <145914129+daliaabdelwhab@users.noreply.github.com>
2023-10-07 22:36:33 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from './src/components/Navbar';
+import './globals.css'
+
+const Layout = ({  }) => {
+  return (
+    <div>
+      <Navbar />
+     
+      
+    </div>
+  );
+};
+
+export default Layout;
